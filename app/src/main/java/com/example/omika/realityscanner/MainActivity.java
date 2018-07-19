13eca65029b0b9f28d02a5cd5f3dbff0a4cb2a93
ImRohithBajjuri <rohithbajjuri520@gmail.com>
2018-07-19 14:04:13 +0530

    Setting up the environment2.

--- app/src/main/java/com/example/omika/realityscanner/MainActivity.java
+++ app/src/main/java/com/example/omika/realityscanner/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.omika.realityscanner;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ViewPager viewPager;
+    screenswiper swiper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //For now we will only show MainScreen. Once we implement the slide feature, we will include all the screens.
 
 
-      //To load MainScreen.
-     getSupportFragmentManager().beginTransaction().replace(R.id.Allscreenscontainer,new MainScreen(),"MainScreen").commit();
+
+
+
+     //This will allow to swipe along the screens.
+     swiper=new screenswiper(getSupportFragmentManager());
+
+     viewPager =findViewById(R.id.allscreenscontainer);
+     viewPager.setAdapter(swiper);
+
+     //Assigning MainScreen as default screen(Screen to show when the app is opened).
+     viewPager.setCurrentItem(1);
+
     }
 }
---
+++ app/src/main/java/com/example/omika/realityscanner/screenswiper.java
@@ -0,0 +1,38 @@
+package com.example.omika.realityscanner;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+//This is an adapter which allows us to swipe between the fragments.
+public class screenswiper extends FragmentPagerAdapter {
+    private static final int screens=3;
+    public screenswiper(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment;
+        if (position==0){
+            fragment=new ProcessingScreen();
+          return fragment;
+        }
+        if (position==1){
+            fragment=new MainScreen();
+            return fragment;
+        }
+        if (position==2){
+            fragment=new CollectionScreen();
+            return fragment;
+        }
+
+       return null;
+    }
+
+    @Override
+    public int getCount() {
+        return screens;
+    }
+}
