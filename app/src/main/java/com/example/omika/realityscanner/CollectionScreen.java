1d742575e705a448671062e57cd8b96dbe5d415f
Anish00 <saianish002@gmail.com>
2018-08-21 00:34:52 +0530

    author

--- app/src/main/java/com/example/omika/realityscanner/CollectionScreen.java
+++ app/src/main/java/com/example/omika/realityscanner/CollectionScreen.java
@@ -1,19 +1,29 @@
 package com.example.omika.realityscanner;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CollectionScreen extends Fragment {
+    private static final String TAG = "MainActivity";
 
+    //vars
+    private static final int NUM_COLUMNS = 2;
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     public CollectionScreen() {
         // Required empty public constructor
@@ -23,14 +33,33 @@ public class CollectionScreen extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        //Collection screen to show all the saved files.
-
-
         View rootview= inflater.inflate(R.layout.fragment_collection_screen, container, false);
+        RecyclerView recyclerView = rootview.findViewById(R.id.recyclerView);
+        collectionsattributeadapter staggeredRecyclerViewAdapter =
+                new collectionsattributeadapter(getContext(), mImageUrls);
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(staggeredRecyclerViewAdapter);
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
 
-    return rootview;
+        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
 
+        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+
+        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+
+
+        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+
+        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
+
+
+        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
+
+        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
+
+        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
+        return rootview;
     }
 
 }
---
+++ app/src/main/java/com/example/omika/realityscanner/collectionsattributeadapter.java
@@ -0,0 +1,74 @@
+package com.example.omika.realityscanner;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.media.CamcorderProfile.get;
+
+public class collectionsattributeadapter extends RecyclerView.Adapter<collectionsattributeadapter.ViewHolder> {
+    private static final String TAG = "StaggeredRecyclerViewAd";
+
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public collectionsattributeadapter(Context context, ArrayList<String> imageUrls) {
+        mImageUrls = imageUrls;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.collectionscreenholder, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(mContext)
+                .load(mImageUrls.get(position))
+                .apply(requestOptions)
+                .into(holder.image);
+
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.image = itemView.findViewById(R.id.imageviewp);
+
+        }
+    }
+}
